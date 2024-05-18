c61d4a233be4c66a12a3940e87801414d12ae0ba
AnhAI12 <leduyanh330@gmail.com>
2024-05-18 23:47:26 +0700

    Anh: Code test

--- deviceDriver/driver_ADC.c
+++ deviceDriver/driver_ADC.c
@@ -5,7 +5,14 @@
 #include<linux/fs.h> // file operation
 #include<linux/proc_fs.h>
 #include<linux/types.h> //dev_t
+#include<linux/device.h>
 
+#defind DRIVER_NAME "driver_ADC"
+#defind DRIVER_CLASS "MyModuleClass"
+
+static dev_t my_device_nr;
+static struct class *my_class;
+static struct cdev my_device;	//why need?
 
 static struct file_operations fops = {
 	.owner = THIS_MODULE,
@@ -42,12 +49,43 @@ static ssize_t driverADC_write(struct file *File, const char *user_buff, size_t 
 static int driverADC_init(void){
 	printk("Init driver ADC \n");
 	
+	//register driver
+	if( (alloc_chrdev_region(&my_device_nr,0,1,DRIVER_NAME) <0 )
+	{ printk("Device register fail\n");
+		return -1;
+	}
+	printk("My driver: major = %d, minor = %d \n", my_device_nr>>20 , my_device_nr & 0xfffff);
+	
+	// create class
+	if( (my_class = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL)
+	{
+		printk("Device class not be created\n");
+		unregister_chrdev_region(my_device_nr,1); return -1;
+	}
+	
+	//create device file
+	if(device_create(my_class, NULL, my_device_nr, NULL, DRIVER_NAME) ==NULL)
+	{
+		printk("can not create device file \n");
+		class_destroy(my_class); return -1;
+	}
+	
+	cdev_init(&my_device, &fops);
+	//register device
+	if(cdev_add(&my_device, my_device_nr, 1)==1){
+		printk("registering device fail\n");
+		device_destroy(my_class, my_device_nr); return -1;
+	}
 	return 0;
 }
 
 static int driverADC_exit(void){
 	printk("close driver ADC \n");
 	
+	cdev_del(&my_device); //
+	device_destroy(my_class, my_device_nr); // delete device
+	class_destroy(my_class); //delete class
+	unregister_chrdev_region(my_device_nr,1) //delete minor
 	return 0;
 }
 
