282c15cdf0f176c7266415df5d165377b37a9327
AnhAI12 <leduyanh330@gmail.com>
2024-05-21 23:38:53 +0700

    abc

---
+++ ds3231Driver/ds3231.c
@@ -0,0 +1,20 @@
+/*
+	This is driver file for Real time DS3231 module
+	Protocol: I2C
+
+*/
+
+#include <stdint.h>
+#
+
+
+//Open device driver
+
+
+//function write
+
+//set up ds3231
+
+//function: write data to ds3231
+
+//function: read data to ds3231
